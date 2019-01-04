f21d12f627149f739c7f580c087d780ad5120e94
zeo1 <43313300+zeo1@users.noreply.github.com>
2019-01-05 05:32:19 +0800

    reset

--- src/data/reset.js
+++ src/data/reset.js
@@ -1,48 +1,46 @@
 export default function(l) {
-  // l.clear()
-  let a = l.findOne({ name: 'desktop' })
-  if (!a)
-    l.insert([
-      {
-        name: 'desktop',
-        tags: 'setting keymap',
-        notion: `m q 
+  l.clear()
+  l.insert([
+    {
+      name: 'desktop',
+      tags: 'setting keymap',
+      notion: `m q 
 t q today
 k q setting::`
-      },
-      {
-        name: 'global',
-        tags: 'setting keymap',
-        notion: `ac1 kmap desktop`
-      },
-      {
-        name: 'note_nmap',
-        tags: 'setting keymap',
-        notion: `i focus
+    },
+    {
+      name: 'global',
+      tags: 'setting keymap',
+      notion: `ac1 kmap desktop`
+    },
+    {
+      name: 'note_nmap',
+      tags: 'setting keymap',
+      notion: `i focus
       esc open_last`
-      },
-      {
-        name: 'note_imap',
-        tags: 'setting keymap',
-        notion: `esc blur`
-      },
-      {
-        name: 'query_imap',
-        tags: 'setting keymap',
-        notion: `up select_up
+    },
+    {
+      name: 'note_imap',
+      tags: 'setting keymap',
+      notion: `esc blur`
+    },
+    {
+      name: 'query_imap',
+      tags: 'setting keymap',
+      notion: `up select_up
 dn select_dn
 ent open
 tab create`
-      },
-      {
-        name: 'query_nmap',
-        tags: 'setting keymap',
-        notion: `i focus`
-      },
-      {
-        name: 'kanban_nmap',
-        tags: 'setting keymap',
-        notion: `ent open_note
+    },
+    {
+      name: 'query_nmap',
+      tags: 'setting keymap',
+      notion: `i focus`
+    },
+    {
+      name: 'kanban_nmap',
+      tags: 'setting keymap',
+      notion: `ent open_note
 a add
 r rm
 s mv_lft
@@ -59,17 +57,17 @@ esc open_last
 x last_day
 c next_day
     `
-      },
-      {
-        name: 'kanban_imap',
-        tags: 'setting keymap',
-        notion: `up focus_up 1
+    },
+    {
+      name: 'kanban_imap',
+      tags: 'setting keymap',
+      notion: `up focus_up 1
 dn focus_dn 1
 mup focus_lft 1
 mdn focus_rit 1
 esc blur
 ent open_note`
-      }
-    ])
+    }
+  ])
   console.log('init local data with default settings')
 }
